0fc5db594f62c049779b9e9405358b9fd2a573c4
CaptainLevi101 <93577803+CaptainLevi101@users.noreply.github.com>
2023-09-26 08:30:13 +0000

    CombinationSum2

---
+++ recursion/CombinationSum2.cpp
@@ -0,0 +1,49 @@
+// Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+
+// Each number in candidates may only be used once in the combination.
+
+// Note: The solution set must not contain duplicate combinations.
+
+ 
+
+// Example 1:
+
+// Input: candidates = [10,1,2,7,6,1,5], target = 8
+// Output: 
+// [
+// [1,1,6],
+// [1,2,5],
+// [1,7],
+// [2,6]
+// ]
+// Example 2:
+
+// Input: candidates = [2,5,2,1,2], target = 5
+// Output: 
+// [
+// [1,2,2],
+// [5]
+// ]
+ class Solution {
+public:
+void find(int ind,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds){
+    if(target==0){
+        ans.push_back(ds);
+        return;
+    }
+    for(int i=ind;i<arr.size();i++){
+        if(i>ind && arr[i]==arr[i-1])continue;
+        if(arr[i]>target)break;
+        ds.push_back(arr[i]);
+        find(i+1,target-arr[i],arr,ans,ds);
+        ds.pop_back();
+    }
+}
+    vector<vector<int>> combinationSum2(vector<int>& cand, int target) {
+        sort(cand.begin(),cand.end()); //as i want answer in sorted order
+         vector<vector<int>>ans;
+         vector<int>ds;
+         find(0,target,cand,ans,ds);
+         return ans;
+    }
+};
